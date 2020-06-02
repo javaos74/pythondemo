80ba326e9b0102be52c7314ff47b7e7d027c2e5d
Hyungsoo Kim <hyungsoo.kim@uipath.com>
2020-06-02 09:06:34 +0900

    최초 등록

---
+++ python/HelloWorld.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+with open('Result.txt', 'w+', encoding='utf-8') as f:
+    f.write( '안녕하세요')
+print( "안녕하세요.")
---
+++ python/greeting.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+def greeting( dir):
+    with open( dir + '\\'  + 'greeting.txt', 'w+' , encoding='utf-8') as f:
+        f.write('안녕하세요. 객체 호출')
+    return '성공'
